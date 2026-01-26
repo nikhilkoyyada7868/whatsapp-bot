f4b6626a9b3686121de63751a94e572304fa9478
Nikhil Koyyada <nikhil.kinvests@gmail.com>
2026-01-26 16:55:52 +0530

    Add 9-emotion mood prompt

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/whatsapp", (req, res) => {
     const twiml = new twilio.twiml.MessagingResponse();
-    twiml.message("Webhook connected ✅");
+    twiml.message("Welcome to whatsapp bot created by Nikhil Koyyada ");
+    twiml.message(
+        "How’s your mood today?\n\n" +
+        "1) Happy\n" +
+        "2) Sad\n" +
+        "3) Angry\n" +
+        "4) Anxious\n" +
+        "5) Calm\n" +
+        "6) Excited\n" +
+        "7) Tired\n" +
+        "8) Stressed\n" +
+        "9) Grateful\n\n" +
+        "Reply with a number (1-9)."
+    );
     res.type("text/xml").send(twiml.toString());
 });
 
